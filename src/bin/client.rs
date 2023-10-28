ed309ae95b34780ee5176efa92c30590120b1e96
Kevin ALBRECHT <bubbuls@gmail.com>
2023-10-28 19:09:15 +0900

    Fix client async read and write

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,110 +1,77 @@
-use std::fmt::write;
-use std::io::stdin;
-use std::io::stdout;
-use std::io::Write;
-use std::net::SocketAddr;
-use std::str;
-
+use std::io::{stdin, stdout, Write};
 use tokio::{
-    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
-    net::TcpStream,
-    sync::broadcast::{self, Receiver, Sender},
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{tcp::ReadHalf, tcp::WriteHalf, TcpStream},
+    sync::mpsc,
 };
 
-const TCP_ADDR: &str = "127.0.0.1:3000";
-
-// #[tokio::main]
-async fn main2() {
-    let mut stream = TcpStream::connect("localhost:3000").await.unwrap();
-
-    let mut read_buffer = Vec::new();
-
-    loop {
-        let (mut reader, mut writer) = stream.split();
-        let entry: String;
-
-        tokio::select! {
-            read_length = reader.read_to_end(&mut read_buffer)=>{
-
-                println!("LOOOOO {:?}",read_length);
-
-                match read_length {
-                    Ok(_)=>{
-                        let lowercase_str = String::from_utf8(read_buffer.clone()).unwrap_or_default().to_ascii_lowercase();
-                        println!(":{}", lowercase_str);
-                    }
-                    Err(e) =>{
-                        eprintln!("Error when reading stream: {}",e);
-                    }
-                }
-            }
-            bytes_written = {
-                entry = read_user_entry().await;
-                writer.write_all(entry.as_bytes())
-            }=>{
-                match bytes_written {
-                    Ok(_)=>{
-                        stream.flush().await;
-                    }
-                    Err(e)=>{
-                        eprintln!("Error when writting on stream: {}",e);
-                    }
-                }
-
-                }
-        }
-
-        // let mut read_buffer = Vec::new();
-        // stream.read(&mut read_buffer).await;
-
-        // println!(":{:?}", String::from_utf8(read_buffer.to_ascii_lowercase()));
-
-        // loop {
-        //     let entry = read_user_entry();
-        //     let writter_bytes = stream.write(entry.as_bytes()).await.unwrap();
-
-        //     if writter_bytes < entry.len() {
-        //         println!("Data lost");
-        //     } else {
-        //         stream.flush();
-        //     }
-        // }
-    }
-}
-
-async fn read_user_entry() -> String {
-    return tokio::task::spawn_blocking(|| {
-        let mut buffer = String::new();
-        stdin().read_line(&mut buffer).unwrap();
-
-        print!("\x1b[1A\x1b[K");
-        stdout().flush().unwrap();
-
-        return buffer;
-    })
-    .await
-    .unwrap();
-}
+const TCP_ADDR: &str = "localhost:3000";
+const BUF_SIZE: usize = 4096;
+const QUEUE_SIZE: usize = 16;
 
 #[tokio::main]
 async fn main() {
     let mut stream = TcpStream::connect(TCP_ADDR).await.unwrap();
     let (reader, mut writer) = stream.split();
+    let (tx, rx) = mpsc::channel(QUEUE_SIZE);
 
-    let read_handle = tokio::spawn(async move {
-        // let (reader, mut writer) = stream.split();
-        let mut reader = BufReader::new(reader);
+    start_reading_user_entries(tx);
+    start_tcp_read_write(reader, &mut writer, rx).await;
+}
+
+fn start_reading_user_entries(tx: mpsc::Sender<String>) {
+    // Tâche pour lire les entrées de l'utilisateur
+    tokio::spawn(async move {
         loop {
-            let mut buf = String::new();
-            if reader.read_line(&mut buf).await.unwrap() > 0 {
-                println!("→{}", buf.trim());
-            }
+            let input = read_user_entry().await;
+            tx.send(input).await.expect("Failed to send user input");
         }
     });
+}
 
+async fn start_tcp_read_write(
+    mut reader: ReadHalf<'_>,
+    writer: &mut WriteHalf<'_>,
+    mut rx: mpsc::Receiver<String>,
+) {
     loop {
-        let mut buf = String::new();
-        stdin().read_line(&mut buf);
-        writer.write_all(buf.as_bytes()).await;
+        let mut read_buffer = [0; BUF_SIZE];
+
+        tokio::select! {
+            read_length = reader.read(&mut read_buffer) => {
+                match read_length {
+                    Ok(0) => {
+                        println!("Connection closed by server.");
+                        return;
+                    }
+                    Ok(len) => {
+                        let received = String::from_utf8_lossy(&read_buffer[..len]);
+                        println!("{}: {}", "Toto", received);
+                    }
+                    Err(e) => {
+                        eprintln!("Error when reading from stream: {}", e);
+                    }
+                }
+            }
+            input_opt = rx.recv() => {
+                if let Some(input) = input_opt {
+                    writer.write_all(input.as_bytes()).await.expect("Failed to write to stream");
+                    stdout().flush().expect("Failed to flush stdout");
+                }
+            }
+        }
     }
+}
+
+async fn read_user_entry() -> String {
+    stdout().flush().expect("Failed to flush stdout");
+    tokio::task::spawn_blocking(|| {
+        let mut buffer = String::new();
+        stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read from stdin");
+        buffer
+    })
+    .await
+    .expect("Failed to join task")
 }
