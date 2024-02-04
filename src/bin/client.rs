83a5cc37bf0e5c8bae5f45084b62e3a0fb61c5fb
Kevin Albrecht <bubbuls@gmail.com>
2024-02-04 16:53:44 +0900

    feat: Migrate client to module

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,27 +1,22 @@
-use arboard::Clipboard;
 use messaging_playground::{client, shared};
 use prost::Message;
-use std::{
-    io::{stdin, stdout, Write},
-    process::exit,
-};
+use std::io::{stdout, Write};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
-    net::{tcp::ReadHalf, tcp::WriteHalf, TcpStream},
+    net::TcpStream,
     sync::mpsc,
 };
 
 const TCP_ADDR: &str = "localhost:3000";
-const BUF_SIZE: usize = 4096;
 const QUEUE_SIZE: usize = 16;
 
 #[tokio::main]
 async fn main() {
     let mut stream = TcpStream::connect(TCP_ADDR).await.unwrap();
-    let (reader, mut writer) = stream.split();
-    let (tx, rx) = mpsc::channel(QUEUE_SIZE);
+    let (mut reader, mut writer) = stream.split();
+    let (tx, mut rx) = mpsc::channel(QUEUE_SIZE);
 
-    let set_username_line: String = ask_username();
+    let set_username_line: String = client::ask_username();
     let mut buf: Vec<u8> = Vec::new();
 
     shared::models::Message {
@@ -40,104 +35,25 @@ async fn main() {
 
     stdout().flush().expect("Failed to flush stdout");
 
-    start_reading_user_entries(tx);
-    start_tcp_read_write(reader, &mut writer, rx).await;
-}
+    client::start_reading_user_entries(tx);
 
-fn start_reading_user_entries(tx: mpsc::Sender<String>) {
-    tokio::spawn(async move {
-        loop {
-            let input = handle_command(read_user_entry().await).trim().to_string();
-            tx.send(input).await.expect("Failed to send user input");
-        }
-    });
-}
-
-async fn start_tcp_read_write(
-    mut reader: ReadHalf<'_>,
-    writer: &mut WriteHalf<'_>,
-    mut rx: mpsc::Receiver<String>,
-) {
     loop {
-        let mut read_buffer = [0; BUF_SIZE];
+        let mut read_buffer = [0; client::BUF_SIZE];
 
         tokio::select! {
-            read_length = reader.read(&mut read_buffer) => {
-                match read_length {
-                    Ok(0) => {
-                        println!("Connection closed by server.");
-                        return;
-                    }
-                    Ok(len) => {
-                        let received = shared::models::Message::decode(&read_buffer[..len]).unwrap();
-                        println!("{}: {}", received.sender, received.message);
-                    }
-                    Err(e) => {
-                        eprintln!("Error when reading from stream: {}", e);
-                        exit(0)
-                    }
-                }
-            }
+            read_length = reader.read(&mut read_buffer) => client::on_message_received(read_length, read_buffer),
             input_opt = rx.recv() => {
-                if let Some(input) = input_opt {
-                    let mut buf = Vec::new();
+                let buf =
+                client::on_mpsc_recv(input_opt).await;
 
-                    shared::models::Message {
-                        message: input,
-                        sender: String::new(),
-                        msg_type: shared::models::message::Type::Broadcast as i32,
-                        recipient: None,
-                    }.encode(&mut buf).unwrap();
-
-                    writer.write_all(&buf).await.expect("Failed to write to stream");
-                    stdout().flush().expect("Failed to flush stdout");
+                if let Some(buff) = buf {
+                    writer
+                    .write_all(&buff)
+                    .await
+                    .expect("Failed to write to stream");
+                stdout().flush().expect("Failed to flush stdout");
                 }
-            }
+            },
         }
     }
-}
-
-async fn read_user_entry() -> String {
-    stdout().flush().expect("Failed to flush stdout");
-    tokio::task::spawn_blocking(|| {
-        let mut buffer = String::new();
-        stdin()
-            .read_line(&mut buffer)
-            .expect("Failed to read from stdin");
-        print!("\x1b[1A\x1b[K");
-
-        buffer
-    })
-    .await
-    .expect("Failed to join task")
-}
-
-fn ask_username() -> String {
-    let mut username = String::new();
-    println!("Enter your username: ");
-    stdin().read_line(&mut username).unwrap();
-
-    let mut name = "/nick ".to_string();
-    name.push_str(username.trim());
-    name.push_str("\r\n");
-
-    name
-}
-
-fn handle_command(input: String) -> String {
-    if input.starts_with('/') {
-        let parts: Vec<&str> = input.split_whitespace().collect();
-        let command_str = parts[0];
-
-        if let Some(command) = client::Command::from_str(command_str) {
-            return match command {
-                client::Command::Paste => {
-                    let mut clipboard = Clipboard::new().unwrap();
-                    clipboard.get_text().unwrap()
-                }
-            };
-        }
-    }
-
-    input
 }
--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,4 +1,18 @@
-pub enum Command {
+use arboard::Clipboard;
+use std::{
+    future::Future,
+    io::{stdin, stdout, Error, Write},
+    process::exit,
+};
+use tokio::sync::mpsc;
+use tokio::task::futures;
+
+use crate::shared;
+use prost::Message;
+
+pub const BUF_SIZE: usize = 4096;
+
+enum Command {
     Paste,
 }
 
@@ -9,4 +23,94 @@ impl Command {
             _ => None,
         }
     }
+}
+
+pub fn ask_username() -> String {
+    let mut username = String::new();
+    println!("Enter your username: ");
+    stdin().read_line(&mut username).unwrap();
+
+    let mut name = "/nick ".to_string();
+    name.push_str(username.trim());
+    name.push_str("\r\n");
+
+    name
+}
+
+pub fn handle_command(input: String) -> String {
+    if input.starts_with('/') {
+        let parts: Vec<&str> = input.split_whitespace().collect();
+        let command_str = parts[0];
+
+        if let Some(command) = Command::from_str(command_str) {
+            return match command {
+                Command::Paste => {
+                    let mut clipboard = Clipboard::new().unwrap();
+                    clipboard.get_text().unwrap()
+                }
+            };
+        }
+    }
+
+    input
+}
+
+pub fn start_reading_user_entries(tx: mpsc::Sender<String>) {
+    tokio::spawn(async move {
+        loop {
+            let input = handle_command(read_user_entry().await).trim().to_string();
+            tx.send(input).await.expect("Failed to send user input");
+        }
+    });
+}
+
+pub async fn read_user_entry() -> String {
+    stdout().flush().expect("Failed to flush stdout");
+    tokio::task::spawn_blocking(|| {
+        let mut buffer = String::new();
+        stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read from stdin");
+        print!("\x1b[1A\x1b[K");
+
+        buffer
+    })
+    .await
+    .expect("Failed to join task")
+}
+
+pub fn on_message_received(len: Result<usize, Error>, buf: [u8; BUF_SIZE]) {
+    return match len {
+        Ok(0) => {
+            println!("Connection closed by server.");
+            return;
+        }
+        Ok(len) => {
+            let received = shared::models::Message::decode(&buf[..len]).unwrap();
+            println!("{}: {}", received.sender, received.message);
+        }
+        Err(e) => {
+            eprintln!("Error when reading from stream: {}", e);
+            exit(0)
+        }
+    };
+}
+
+pub async fn on_mpsc_recv(input_opt: Option<String>) -> Option<Vec<u8>> {
+    if let Some(input) = input_opt {
+        let mut buf = Vec::new();
+
+        shared::models::Message {
+            message: input,
+            sender: String::new(),
+            msg_type: shared::models::message::Type::Broadcast as i32,
+            recipient: None,
+        }
+        .encode(&mut buf)
+        .unwrap();
+
+        return Some(buf);
+    }
+
+    None
 }
